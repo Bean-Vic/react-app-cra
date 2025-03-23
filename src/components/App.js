dec55c23fbeb2a4d98f8805c6954d4693aaa3f6e
Bean <devmastersj@gmail.com>
2025-03-23 17:30:24 -0500

    add more starter code for redux and saga

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+// App.js
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../reducers/counterSlice';
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+      <div>
+        <h1>Counter: {count}</h1>
+
+        <button onClick={() => dispatch({ type: 'counter/asyncIncrement' })}>
+          Async +1 (via Saga)
+        </button>
+        <button onClick={() => dispatch({ type: 'counter/asyncDecrement' })}>
+          Async -1 (via Saga)
+        </button>
+        <button onClick={() => dispatch(increment())}>+1</button>
+        <button onClick={() => dispatch(decrement())}>-1</button>
+      </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './sass/index.scss';
-import App from './js/App';
+import App from './components/App';
+import { Provider } from 'react-redux'
+import store from './store'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   </React.StrictMode>
 );
