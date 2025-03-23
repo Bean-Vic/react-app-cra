b63ed4de4c34dd67d710549c5aec353ef659f163
Bean <devmastersj@gmail.com>
2025-03-23 17:50:40 -0500

    config jest to tests folder

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,17 @@
 const config = {
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    roots: ['<rootDir>/tests'],
+    setupFilesAfterEnv: ['<rootDir>/tests/setupTests.js'],
+    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+    transform: {
+        '^.+\\.[jt]sx?$': 'babel-jest',
+    },
     moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/src/$1',
         '^js/(.*)$': '<rootDir>/src/js/$1',
         '^components/(.*)$': '<rootDir>/src/components/$1',
-        // add more aliases as needed
+        // add more if needed
     },
-    moduleDirectories: ['node_modules', 'src'], // allow absolute imports from src/
+    moduleDirectories: ['node_modules', 'src'],
 }
 
 module.exports = config;
